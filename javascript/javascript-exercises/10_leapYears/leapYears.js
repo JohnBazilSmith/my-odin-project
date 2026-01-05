aff4822c16592bf51f4867aacdbb7a9ef525ad1c
JohnBazilSmith <212019201+JohnBazilSmith@users.noreply.github.com>
2026-01-06 00:30:14 +0200

    did JS exercises 10 and 11

--- javascript/javascript-exercises/10_leapYears/leapYears.js
+++ javascript/javascript-exercises/10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0
+        || year % 400 === 0) {
+        return `${year} is a leap year!`;
+    } else {
+        return `${year} is not a leap year!`;
+    };
 };
 
+let checkedYear = 1923;
+console.log(leapYears(checkedYear));
 // Do not edit below this line
 module.exports = leapYears;
--- javascript/javascript-exercises/11_tempConversion/tempConversion.js
+++ javascript/javascript-exercises/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let C = 5 * (f - 32) / 9;
+  return `${f} degrees Fahrenheit is the same as ${C.toFixed(1)} degrees Celsius`;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let f = 9 * C / 5 + 32;
+  return `${C} degrees Celsius is the same as ${f.toFixed(1)} degrees Fahrenheit`;
 };
 
 // Do not edit below this line
